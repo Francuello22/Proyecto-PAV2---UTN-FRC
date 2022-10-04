fb35ac1acd93d26e7543d72e7313cb884bcf0fb0
Francuello22 <102491587+Francuello22@users.noreply.github.com>
2022-10-04 19:25:10 -0300

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
@@ -8,20 +8,32 @@ import { APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ArticuloFamiliasComponent } from './components/articulo-familias/articulo-familias.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
 
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbPaginationModule, HttpClientModule, 
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    NgbPaginationModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/inicio', pathMatch: 'full' },
       { path: 'inicio', component: InicioComponent },
       { path: 'articulosfamilias', component: ArticuloFamiliasComponent },
-    ])
+      { path: 'articulos', component: ArticulosComponent },
+    ]),
   ],
-  declarations: [AppComponent, InicioComponent, ArticuloFamiliasComponent],
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    ArticuloFamiliasComponent,
+    ArticulosComponent,
+    MenuComponent,
+  ],
   providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent],
-  
 })
 export class AppModule {}
